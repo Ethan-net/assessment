531bd2318cc244c5483ae2f15d4b9c191000417d
Akorede Ethan <koredeajibola@gmail.com>
2025-05-13 11:17:55 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
+import React from "react";
+
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Aboutme from "./pages/AboutMe";
 import Navbar from "./components/Navbar";
 
 function App() {
-  <>
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Aboutme />} />
-      </Routes>
-    </BrowserRouter>
-  </>;
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Aboutme />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import Aboutme from "../pages/AboutMe";
+import Skills from "../pages/Skills";
+import Hobbies from "../pages/Hobbies";
+import Passion from "../pages/Passion";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
@@ -9,7 +13,18 @@ export default function Navbar() {
           <strong className="bg-blue-400 p-2 rounded-full">A</strong>jibola
         </h1>
       </div>
-      <Aboutme />
+      <Link to="/">
+        <Aboutme />
+      </Link>
+      <Link to="/skills">
+        <Skills />
+      </Link>
+      <Link to="/hobbies">
+        <Hobbies />
+      </Link>
+      <Link to="/assion">
+        <Passion />
+      </Link>
     </div>
   );
 }
